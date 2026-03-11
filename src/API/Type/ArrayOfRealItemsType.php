7618ec49ba20390b8c0f5f7094b9df6e7bed6031
Gareth Parker <blackfireweb@gmail.com>
2026-03-11 20:23:22 +0000

    Add missing return types for Countable, ArrayAccess, IteratorAggregate (#286)

--- src/API/Type/ArrayOfRealItemsType.php
+++ src/API/Type/ArrayOfRealItemsType.php
@@ -117,23 +117,23 @@ class ArrayOfRealItemsType extends Type implements Countable, ArrayAccess, Itera
         return $this->itemsArray;
     }
 
-    public function count()
+    public function count(): int
     {
         return count($this->getItems());
     }
 
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->getItems()[$offset]);
     }
 
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         $this->getItems();
         return isset($this->itemsArray[$offset]) ? $this->itemsArray[$offset] : null;
     }
 
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         $this->getItems();
 
@@ -144,13 +144,13 @@ class ArrayOfRealItemsType extends Type implements Countable, ArrayAccess, Itera
         }
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         $this->getItems();
         unset($this->itemsArray[$offset]);
     }
 
-    public function getIterator()
+    public function getIterator(): \Traversable
     {
         $this->getItems();
         return new \ArrayIterator($this->itemsArray);
